220443e4acd4e3787676393763b2de766479b8e7
Ariel013 <kevin.sodjinou@epitech.eu>
2023-11-27 21:41:16 +0100

    [ADD] PLayer view

---
+++ frontend/tsi_app/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import Player from './Components/Player';
+// import './App.css';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Player/>
+    </div>
+  );
+}
+
+export default App;
