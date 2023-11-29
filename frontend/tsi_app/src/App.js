fadcdfb5eea1b9e88cf6d1d615c9bb83cdafac7f
Ariel013 <kevin.sodjinou@epitech.eu>
2023-11-29 04:47:44 +0100

    [ADD] Admin Dashboard Page

--- frontend/tsi_app/src/App.js
+++ frontend/tsi_app/src/App.js
@@ -2,6 +2,16 @@
 import Player from './pages/Player/Player';
 import SignupPage from './pages/Signup';
 import LoginPage from './pages/Login';
+import Users from './components/Users';
+// import Dashboard from './pages/Dashboard';
+import {
+  UserCircle,
+  LayoutDashboard,
+  Boxes,
+} from "lucide-react"
+import Sidebar from './components/Sidebar';
+import { SidebarItem } from './components/Sidebar';
+
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 // import './App.css';
 function App() {
@@ -12,7 +22,15 @@ function App() {
           <Route path="/" element={<Player />} />
           <Route path="/register" element={<SignupPage />} />
           <Route path="/signup" element={<LoginPage />} />
+          <Route path="/users" element={<Users />} />
+          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
       </Routes>
+      <Sidebar>
+            <SidebarItem icon={<LayoutDashboard size={20} />} text="Dashboard" alert />
+            <SidebarItem icon={<UserCircle size={20} />} text="Users" />
+            <SidebarItem icon={<UserCircle size={20} />} text="Players" />
+            <SidebarItem icon={<Boxes size={20} />} text="Games" />
+        </Sidebar>
     </BrowserRouter>
   );
 }
